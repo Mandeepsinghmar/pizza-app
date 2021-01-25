5d7d4f6d030e0653d8f646722cc38dc915934719
Mandeep Singhmar <msigh8328@gmail.com>
2021-01-25 16:14:44 +0530

    add:styled sidebar

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Navbar } from '../Navbar';
+import { Sidebar } from '../Sidebar';
 import { HeroContainer, HeroContent, HeroItems, HeroH1, HeroBtn, HeroP} from './HeroElements';
 
 export const Hero = () => {
     return (
        <HeroContainer>
            <Navbar />
+           <Sidebar />
             <HeroContent>  
                <HeroItems>
                    <HeroH1>Greatest Pizza Ever</HeroH1>

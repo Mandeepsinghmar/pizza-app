8ff98647c178b3b50192f9a9fee8312b60c67148
Mandeep Singhmar <msigh8328@gmail.com>
2021-01-25 17:37:50 +0530

    add:comlete component-sidebar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Nav, NavLink, NavIcon, Bars } from './NavbarElements';
 
- export const Navbar = () => {
+ export const Navbar = ({toggle}) => {
     return (
        <Router>
             <Nav>
                 <NavLink to="/">Pizza</NavLink>
-                <NavIcon>
+                <NavIcon onClick={toggle}>
                 <p>Menu</p>
                 <Bars />
                 </NavIcon>

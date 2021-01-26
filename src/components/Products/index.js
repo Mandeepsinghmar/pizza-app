e2386e75ec184d841e8a47a71e45e8e0dafce38e
Mandeep Singhmar <msigh8328@gmail.com>
2021-01-26 15:48:29 +0530

    product  mapping

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+import {
+    ProductsContainer, 
+    ProductsHeading,
+    ProductsWrapper,
+    ProductCard,
+    ProductImg,
+    ProductInfo,
+    ProductTitle,
+    ProductDesc,
+    ProductPrice,
+    ProductButton
+} from './ProductsElements';
 
 export const Products = () => {
     return (
         <ProductsContainer>
             <ProductsHeading>Heading</ProductsHeading>
             <ProductsWrapper>
-                
+                {data.map((product, index) => {
+                    return (
+                        <ProductCard key={index}>
+                            <ProductImg src={product.img} alt={product.alt} />
+                            <ProductInfo>
+                                <ProductTitle>{product.name}</ProductTitle>
+                                <ProductDesc>{product.desc}</ProductDesc>
+                                <ProductPrice>{product.price}</ProductPrice>
+                                <ProductButton>{product.button}</ProductButton>
+                            </ProductInfo>
+                        </ProductCard>
+                    )
+                })}
             </ProductsWrapper>
         </ProductsContainer>
     )

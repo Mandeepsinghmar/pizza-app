5faa235d341dfafa83875c235c0d0b1ed8390ef7
Mandeep Singhmar <msigh8328@gmail.com>
2021-01-26 15:00:53 +0530

    add: products folder

---
+++ src/components/Products/data.js
@@ -0,0 +1,10 @@
+export const productData = [
+    {
+        img: product1,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '$19.99',
+        button: 'Add to Cart'
+    },
+]
